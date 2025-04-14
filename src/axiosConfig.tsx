f9e4e685bfe081db1127c9e146e324e6bed610dc
GOVIND SHARMA <96180010+Govindsharma123@users.noreply.github.com>
2025-04-14 10:51:32 +0530

    api baseurl fix

--- src/axiosConfig.tsx
+++ src/axiosConfig.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 // import AsyncStorage from '@react-native-async-storage/async-storage';
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "https://amme-api.vercel.app/api",
+  baseURL: "https://amme-api-pied.vercel.app/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
